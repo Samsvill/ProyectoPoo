a897acf41137ada85539fbd5d2b1db4a763be1a7
Samsvill <samantha.sharid@gmail.com>
2022-01-30 13:55:51 -0500

    Controller mascota creado

---
+++ proyectolInterfazAbSamJo/src/main/java/ec/edu/espol/controllers/MascotaGrafoController.java
@@ -0,0 +1,45 @@
+package ec.edu.espol.controllers;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+/**
+ * FXML Controller class
+ *
+ * @author saman
+ */
+public class MascotaGrafoController implements Initializable {
+
+
+    @FXML
+    private Button Re;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField nombreMascota;
+    @FXML
+    private TextField raza;
+    @FXML
+    private TextField birthday;
+    @FXML
+    private TextField pathimage;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
